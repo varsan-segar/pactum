ec12b50609e17e909aa61bc2c96954a44a5064df
VARSAN S S <varsansegars@gmail.com>
2026-06-05 21:07:46 +0530

    Add Streamlit application interface

---
+++ app.py
@@ -0,0 +1,132 @@
+import uuid
+import streamlit as st
+from streamlit_cookies_controller import CookieController
+from pathlib import Path
+
+from langchain_core.messages import HumanMessage, AIMessage
+
+from core.ingestion import ingestion_pipeline
+from core.document_manager import list_documents, delete_document
+from core.pipeline import rag_pipeline
+
+from security.validators import UnsafeInputError, FileValidationError, validate_query, validate_file
+from config import FILE_UPLOAD_PATH 
+
+st.set_page_config(
+    page_title="RAG Application",
+    page_icon=":robot:",
+    layout="wide"
+)
+
+cookies = CookieController()
+
+if 'user_id' not in st.session_state:
+    user_id = cookies.get("user_id")
+
+    if not user_id:
+        user_id = str(uuid.uuid4())
+        cookies.set("user_id", user_id)
+        st.write(f"USER ID: {user_id}\nThis is your user id, save it to use the app with your documents on different devies or browsers.")
+        
+    st.session_state.user_id = user_id
+        
+if 'history' not in st.session_state:
+    st.session_state.history = []
+
+if 'documents' not in st.session_state:
+    st.session_state.documents = list_documents(user_id=st.session_state.user_id)
+
+st.title("PDF RAG Q&A Application")
+
+with st.sidebar:
+    st.header("DOCUMENTS:")
+
+    file = st.file_uploader(
+        label="Upload PDF File",
+        type=[".pdf"],
+        max_upload_size=20
+    )
+
+    if file:
+        try:
+            is_new_file = True
+
+            for document in st.session_state.documents:
+                if document['file_name'] == file.name:
+                    is_new_file = False
+                    st.warning("This document is already indexed in the Vector DB.")
+
+            if is_new_file:        
+                FILE_PATH = FILE_UPLOAD_PATH / file.name
+
+                with FILE_PATH.open("wb") as f:
+                    f.write(file.getbuffer())
+
+                file = validate_file(file=FILE_PATH)
+
+                status = ingestion_pipeline(file=FILE_PATH, user_id=st.session_state.user_id)
+
+                with st.expander(label="INGESTION STATUS:"):
+                    for k, v in status.items():
+                        st.write(f"{k} -> {v}")
+
+                st.session_state.documents = list_documents(user_id=st.session_state.user_id)                    
+        except FileNotFoundError as e:
+            st.warning(e)
+        except FileValidationError as e:
+            st.warning(f"File Validation Error: {e}")
+            FILE_PATH.unlink()
+    
+    if len(st.session_state.documents) > 0:
+        with st.expander(label="DOCUMENT LIST:"):
+            for document in st.session_state.documents:
+                    st.write(f"{document['file_name']} -> {document['doc_id']}")
+                    if st.button(label="Delete Document", key=f"delete_{document['doc_id']}"):
+                        delete_document(user_id=st.session_state.user_id, doc_id=document['doc_id'])
+                        st.session_state.documents = list_documents(user_id=st.session_state.user_id)
+                        st.rerun()
+
+for msg in st.session_state.history:
+    with st.chat_message(msg['role']):
+        st.markdown(msg['content'])
+
+        if msg['role'] == "assistant":
+            if msg["sources"]:
+                with st.expander(label="SOURCES:"):
+                    for i, chunk in enumerate(msg["sources"]):
+                        st.write(f"{i}. [{chunk.file_name}] [{chunk.page_number}] [{chunk.chunk_index}]")
+                        st.write(f"     {chunk.content}")
+
+prompt = st.chat_input("Enter your query")
+
+if prompt:
+    try:
+        query = validate_query(query=prompt)
+        
+        with st.chat_message("user"):
+            st.markdown(query)
+
+        st.session_state.history.append({"role": "user", "content": query})
+
+        conversation_history = []
+
+        for msg in st.session_state.history:
+            if msg["role"] == "user":
+                conversation_history.append(HumanMessage(msg["content"]))
+            elif msg["role"] == "assistant":
+                conversation_history.append(AIMessage(msg['content']))
+
+        response, chunks = rag_pipeline(user_id=st.session_state.user_id, query=query, history=conversation_history[:-1])
+
+        with st.chat_message("assistant"):
+            full_response = st.write_stream(response)
+
+            if chunks:
+                with st.expander(label="SOURCES:"):
+                    for i, chunk in enumerate(chunks):
+                        st.write(f"{i}. [{chunk.file_name}] [{chunk.page_number}] [{chunk.chunk_index}]")
+                        st.write(f"     {chunk.content}")
+
+        st.session_state.history.append({"role": "assistant", "content": full_response, "sources": chunks})
+    except UnsafeInputError as e:
+        st.warning(f"Unsafe Input Error: {e}")
